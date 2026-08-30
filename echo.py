f076a2459d05aac7e31549569313177b1210c779
Sneha Verma <sneha22verma4@gmail.com>
2026-08-30 18:47:53 +0530

    Update echo.py

--- echo.py
+++ echo.py
@@ -77,9 +77,10 @@ if user_input:
     try:
 
         response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages=st.session_state.messages
-        )
+    model="llama-3.1-8b-instant",
+    messages=st.session_state.messages,
+    temperature=0.7
+)
 
         bot_reply = response.choices[0].message.content
 
